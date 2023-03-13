47cc4bfe058f51cae40d5620ab2f3514464d0868
Jokūbas Rybelis <frizjr@protonmail.com>
2023-03-13 12:33:56 +0200

    Add form validation

--- CurrencyCalculator.Web/CurrencyCalculator-ng/src/app/header/header.component.ts
+++ CurrencyCalculator.Web/CurrencyCalculator-ng/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Currency } from '../currencies/currency';
 import { UserSelectedExchangeDetails } from '../exchange-rates/exchange-rate.utils';
 import {CURRENCY} from './header.component.utils';
@@ -14,6 +15,9 @@ export class HeaderComponent  implements OnInit {
   @Input() currencies: Currency[] = [];
   @Output() submitExchangeRateRequest = new EventEmitter();
 
+  requestForm: FormGroup;
+  imageSource: string = '/assets/currencies.jpg';
+  message: string = 'Converts your selected currency and amount to another currency, for the specified date. One of the exchanged currencies must be euro';
   minDate: Date;
   state: UserSelectedExchangeDetails = {
     selectedCurrencyFrom: '',
@@ -22,15 +26,48 @@ export class HeaderComponent  implements OnInit {
     date: new Date
   };
 
+
+  public get selectedCurrencyFrom() {
+    return this.requestForm.get('selectedCurrencyFrom');
+  }
+
+  public get selectedCurrencyTo() {
+    return this.requestForm.get('selectedCurrencyTo');
+  }
+
+  public get currencyAmount() {
+    return this.requestForm.get('currencyAmount');
+  }
+
+  public get date() {
+    return this.requestForm.get('date');
+  }
+
+
   constructor () {
     this.minDate = new Date("2015-01-01")
   };
 
   ngOnInit() : void {
+    this.requestForm = new FormGroup({
+      selectedCurrencyFrom: new FormControl(null, Validators.required),
+      selectedCurrencyTo: new FormControl(null, Validators.required),
+      currencyAmount: new FormControl(null, Validators.required),
+      date: new FormControl(null, Validators.required)
+    })
   }
 
-  message: string = 'Converts your selected currency and amount to another currency, for the specified date. One of the exchanged currencies must be euro';
-  imageSource: string = '/assets/currencies.jpg';
+  isFormInvalid(): boolean {
+    if (this.requestForm.invalid) {
+      return true;
+    }
+    return false;
+  }
+
+  IsNumeric(input: any)
+  {
+    return (input - 0) == input && (''+input).trim().length > 0;
+  }
 
   checkCurrencyFrom(){
     const CURRENCY_FROM = this.state.selectedCurrencyFrom;
